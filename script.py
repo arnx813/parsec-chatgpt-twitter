d58e89dc98beb6bc03ee06663cf4d741cde61eb9
Aaron <aaronxie2003@gmail.com>
2023-05-14 12:18:37 -0400

    stuff

--- script.py
+++ script.py
@@ -1,16 +1,20 @@
 import os
 import openai
 
-openai.api_key = "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"
+def chatbot_completion():
+    openai.api_key = "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"
 
-print(openai.api_key)
+    print(openai.api_key)
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "user", "content": "Tell the world about the ChatGPT API in the style of a pirate."}
-  ]
-)
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": "Tell the world about the ChatGPT API in the style of a pirate."}
+        ]
+    )
 
-print(completion.choices[0].message.content)
-print('test')
+    print(completion.choices[0].message.content)
+    print('test')
+
+# Call the function
+chatbot_completion()
